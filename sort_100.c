a6a3036b7c9499e91614d577db6912896bccdf19
Ayoub Kadi <akadi@e1r5p8.1337.ma>
2022-04-13 02:51:49 +0000

    up

--- sort_100.c
+++ sort_100.c
@@ -6,23 +6,83 @@
 /*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 21:57:29 by akadi             #+#    #+#             */
-/*   Updated: 2022/04/11 22:52:58 by akadi            ###   ########.fr       */
+/*   Updated: 2022/04/13 02:40:53 by akadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void	sort_100(t_list **stack_a, t_list **stack_b, int len)
+int	find_index(int content, int *arr, int len)
+{
+	int		i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (arr[i] == content)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+t_list	*change_stack_a(t_list **stack_a, int *arr, int len)
 {
 	int	i;
+	int	index;
+	t_list	*p;
 
-	i = len;
-	while (i > 0)
-	{
-		push_smallest(stack_a, stack_b, i);
-		i--;
-	}
 	i = 0;
-	while(++i <= len)
-		push_a(stack_a, stack_b);
+	p = *stack_a;
+	while (i < len)
+	{
+		index = find_index((*stack_a) -> content, arr, len);
+		if (index != -1)
+			(*stack_a) -> content = index;
+		i++;
+		(*stack_a) = (*stack_a) -> next;
+	}
+	return (p);
+}
+int	*sort_array(int *arr, int len)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			if (arr[i] > arr[j])
+			{
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (arr);
+}
+void	sort_100(t_list **stack_a, t_list **stack_b, int len)
+{
+	(void)stack_b;
+	int		i;
+	int		*arr;
+	t_list	*p;
+
+	arr = malloc(sizeof(int) * len);
+	p = *stack_a;
+	i = 0;
+	while (i < len)
+	{
+		arr[i] = p -> content;
+		p = p -> next;
+		i++;
+	}
+	arr = sort_array(arr, len);
+	*stack_a = change_stack_a(stack_a, arr, len);
 }
